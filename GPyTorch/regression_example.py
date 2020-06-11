83725361e8cddf6ad95b731e8553a3e65f319f96
akihiro.y <aki916.genyo@gmail.com>
2020-06-11 22:32:59 +0900

    可読性を高めるように修正

--- GPyTorch/regression_example.py
+++ GPyTorch/regression_example.py
@@ -22,31 +22,7 @@ class ExactGPModel(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-def main():
-    n = 100
-    train_x, train_y = prepare_data(n)
-    # initialize likelihood and model
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    model = ExactGPModel(train_x, train_y, likelihood)
-
-    # this is for running the notebook in our testing framework
-    # import os
-    # smoke_test = ('CI' in os.environ)
-    training_iter = 50
-
-
-    # Find optimal model hyperparameters
-    model.train()
-    likelihood.train()
-
-    # Use the adam optimizer
-    optimizer = torch.optim.Adam([
-        {'params': model.parameters()},  # Includes GaussianLikelihood parameters
-    ], lr=0.1)
-
-    # "Loss" for GPs - the marginal log likelihood
-    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
-
+def train(model, optimizer, mll, train_x, train_y, training_iter):
     for i in range(training_iter):
         # Zero gradients from previous iteration
         optimizer.zero_grad()
@@ -61,28 +37,59 @@ def main():
             model.likelihood.noise.item()
         ))
         optimizer.step()
+    return model
+
+def test(xmin,xmax,model,likelihood):
+    # Test points are regularly spaced along [0,1]
+    # Make predictions by feeding model through likelihood
+    with torch.no_grad(), gpytorch.settings.fast_pred_var():
+        test_x = torch.linspace(xmin, xmax, 51)
+        observed_pred = likelihood(model(test_x))
+    return test_x, observed_pred
+
+def plot(train_x, train_y, test_x, observed_pred):
+    with torch.no_grad():
+        # Get upper and lower confidence bounds(2 standard deviations)
+        lower, upper = observed_pred.confidence_region()
+        # Plot training data as black stars
+        plt.scatter(train_x.numpy(), train_y.numpy(), color='k', marker='*')
+        # Plot predictive means as blue line
+        plt.plot(test_x.numpy(), observed_pred.mean.numpy(), color='b')
+        # Shade between the lower and upper confidence bounds
+        plt.fill_between(test_x.numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
+        plt.legend(['Observed Data', 'Mean', 'Confidence'])
+    plt.show()
+
+
+def main():
+    n = 100
+    train_x, train_y = prepare_data(n)
+    # initialize likelihood and model
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    model = ExactGPModel(train_x, train_y, likelihood)
+
+    training_iter = 50
+
+
+    # Find optimal model hyperparameters
+    model.train()
+    likelihood.train()
+    # Use the adam optimizer
+    optimizer = torch.optim.Adam([{'params': model.parameters()}], lr=0.1)
+    # "Loss" for GPs - the marginal log likelihood
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+    # train
+    model = train(model, optimizer, mll, train_x, train_y, training_iter)
 
     # Get into evaluation (predictive posterior) mode
     model.eval()
     likelihood.eval()
 
-    # Test points are regularly spaced along [0,1]
-    # Make predictions by feeding model through likelihood
-    with torch.no_grad(), gpytorch.settings.fast_pred_var():
-        test_x = torch.linspace(0, 1, 51)
-        observed_pred = likelihood(model(test_x))
+    xmin, xmax = -0.2, 1.2
+    test_x, observed_pred = test(xmin,xmax,model,likelihood)
+    plot(train_x, train_y, test_x, observed_pred)
 
-    with torch.no_grad():
-        # Get upper and lower confidence bounds
-        lower, upper = observed_pred.confidence_region()
-        # Plot training data as black stars
-        plt.plot(train_x.numpy(), train_y.numpy(), 'k*')
-        # Plot predictive means as blue line
-        plt.plot(test_x.numpy(), observed_pred.mean.numpy(), 'b')
-        # Shade between the lower and upper confidence bounds
-        plt.fill_between(test_x.numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
-        plt.legend(['Observed Data', 'Mean', 'Confidence'])
-    plt.show()
 
 
 if __name__ == '__main__':
