6afc5a61517c63763817ff434fe70dca28384a37
akihiro.y <aki916.genyo@gmail.com>
2020-06-22 10:43:31 +0900

    add other model

--- metric_learning/main.py
+++ metric_learning/main.py
@@ -11,14 +11,16 @@ import statistics
 import time
 import argparse
 import matplotlib
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import datetime
 from torch.autograd import grad as torch_grad
 from torchvision.transforms.functional import to_pil_image, to_tensor
-from network import SoftmaxLoss
+from network import *
 import torchvision.models as models
+from torch.nn import functional as F
+
 
 def download_dataset(transform):
     train_dataset = torchvision.datasets.MNIST('./data', train=True,  download=False, 
@@ -34,49 +36,102 @@ def body_feature_model(model):
     body, pool, head = list(model.children()) 
     return body, pool, head[:-1]
 
+def visualize_feature_center(test_dataloader,BB,metric_fc):
+    plt.figure()
+    plt.xlim([-1.1,1.1])
+    plt.ylim([-1.1,1.1])
+    cmap = plt.get_cmap("tab10")
+    count = 10
+    c = 0
+
+    # データの特徴ベクトルの可視化
+    with torch.no_grad():
+        for picture, label in test_dataloader:
+            feature = BB(picture)
+            normalized_feature = F.normalize(feature)
+            sc = plt.scatter(normalized_feature[:,0],normalized_feature[:,1],c=label,cmap=cmap,alpha=0.8)
+            # print()
+            # break
+            c += 1
+            if c == count:
+                break
+
+    # クラスタの中心の可視化
+    normalized_center = F.normalize(metric_fc.weight).detach().numpy()
+    for i in range(10):
+        plt.plot([0,normalized_center[i,0]],[0,normalized_center[i,1]],c=cmap(i))
+    # sc = plt.scatter(normalized_center[:,0],normalized_center[:,1],c=label,cmap=cmap)
+
+    # 2dimならそのまま可視化、それ以上なら次元圧縮
+
+    plt.colorbar(sc)
+    plt.show()
+
+def save_model(model, save_path, name, iter_cnt):
+    save_name = os.path.join(save_path, name + '_' + str(iter_cnt) + '.pth')
+    torch.save(model.state_dict(), save_name)
+    return save_name
+
+def train(train_dataloader,metric_fc, BB, params, n_epoch):
+    # # for i in range(n_epoch):
+    loss_epoch = 0
+    count = 30
+    c = 0
+    loss_CE = nn.CrossEntropyLoss()
+
+    for picture,label in train_dataloader:
+        feature = BB(picture)
+        output = metric_fc(feature, label)
+        print(output)
+        loss = loss_CE(output, label)
+        loss.backward()
+        params.step()
+        loss_epoch += loss.data
+        c += 1
+        if c == count:
+            break
+        print(f"/{n_epoch} train loss: {loss.data}")
+
+    return metric_fc, BB, params
+
 def main():
     transform = transforms.Compose([
         transforms.Resize((224,224), interpolation=2),
-        transforms.Grayscale(num_output_channels=3) ,
-        transforms.ToTensor()#PIL.Image.Image-> torch tensor
+        transforms.Grayscale(num_output_channels=3),
+        transforms.ToTensor(),#PIL.Image.Image-> torch tensor,
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
     ])
 
     train_dataset, test_dataset = download_dataset(transform)
 
     backbone_model = models.alexnet(pretrained=True)
     body, pool, head = body_feature_model(backbone_model)
-    model = SoftmaxLoss(body, pool, head)
-    params = torch.optim.Adam(model.parameters(),lr=0.001)
+    # BB = resnet34()
+    feature_dim = 1000
+    BB = BackBone(body, pool, head, feature_dim)
+    metric_fc = SoftmaxLoss(in_features=feature_dim,out_features=10)
+    # metric_fc = ArcMarginProduct(in_features=feature_dim,out_features=10)
+    params = torch.optim.Adam([{'params': BB.parameters()}, {'params': metric_fc.parameters()}],lr=0.001)
     
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=1)
-    loss_CE = nn.CrossEntropyLoss()
+    test_dataloader  = torch.utils.data.DataLoader(test_dataset,  batch_size=64, shuffle=True, num_workers=1)
 
     n_epoch = 10
-    # for i in range(n_epoch):
-    #     loss_epoch = 0
-    for picture,label in train_dataloader:
-        print(picture.shape)
-        out = model(picture)
-        print(out.shape)
-        exit()
-        loss = loss_CE(out, label)
-        loss.backward()
-        params.step()
-        # loss_epoch += loss.data
-        print(f"/{n_epoch} train loss: {loss.data}")
 
-    # ToDo 中間層の可視化
-    ## 重み行列の特定の行成分の取得
-    # model.Linear2.weight[0]
+    metric_fc, BB, params = train(train_dataloader,metric_fc, BB, params, n_epoch)
+    exit()
 
-    # torch.save
+    # with no grad
+    visualize_feature_center(test_dataloader,BB,metric_fc)
+
+    save_path = './'
+    name = 'SoftmaxLoss'
+    iter_cnt = '1'
+    # save_model(model, save_path, name, iter_cnt)
 
     # validでハイパラを探索する部分
-    # Datasetを自分で作った方が早いかも
-
-
-
-
+    # 特定のクラスを除外することを考えるとDatasetを自分で作った方が早いかも
 
 
 if __name__ == '__main__':
--- metric_learning/network.py
+++ metric_learning/network.py
@@ -1,22 +1,309 @@
 import torch.nn as nn
 from torch.nn import functional as F
 import torch
+import math
+import torch.utils.model_zoo as model_zoo
 
-class SoftmaxLoss(nn.Module):
-    def __init__(self, body, pool, head):
+def resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
+    return model
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+        #                        bias=False)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=2)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        # self.avgpool = nn.AvgPool2d(8, stride=1)
+        # self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc5 = nn.Linear(512 * 8 * 8, 512)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        # x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        # x = nn.AvgPool2d(kernel_size=x.size()[2:])(x)
+        # x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc5(x)
+
+        return x
+
+
+def resnet18(pretrained=True, **kwargs):
+    """Constructs a ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    # if pretrained:
+    model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
+
+class BackBone(nn.Module):
+    def __init__(self, body,pool,head,feature_dim):
         super().__init__()
         self.body = body
         self.pool = pool
         self.head = head
-        self.Linear1 = nn.Linear(4096,100)
-        self.Linear2 = nn.Linear(100,10)
+        self.L1 = nn.Linear(4096,feature_dim)
 
-    def forward(self, x):
+    def forward(self,x):
         x = self.body(x)
         x = self.pool(x)
         x = torch.flatten(x, 1)
         x = self.head(x)
-        x = F.leaky_relu(self.Linear1(x),inplace=True)
-        x = self.Linear2(x)
+        x = F.relu(self.L1(x))
+
+        return x       
+
+
+class SoftmaxLoss(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.weight = nn.Parameter(torch.FloatTensor(out_features,in_features))
+
+    def forward(self, feature, label):
+        x = F.linear(F.normalize(feature), F.normalize(self.weight))
 
         return x
+
+class ArcMarginProduct(nn.Module):
+    r"""Implement of large margin arc distance: :
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            s: norm of input feature
+            m: margin
+            cos(theta + m)
+        """
+    def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False):
+        super(ArcMarginProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, feature, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cosine = F.linear(F.normalize(feature), F.normalize(self.weight))
+        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+        # --------------------------- convert label to one-hot ---------------------------
+        # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
+        # one_hot = torch.zeros(cosine.size(), device='cuda')
+        one_hot = torch.zeros(cosine.size())
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
+        output *= self.s
+        # print(output)
+
+        return output
+
+
+class AddMarginProduct(nn.Module):
+    r"""Implement of large margin cosine distance: :
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        s: norm of input feature
+        m: margin
+        cos(theta) - m
+    """
+
+    def __init__(self, in_features, out_features, s=30.0, m=0.40):
+        super(AddMarginProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, feature, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cosine = F.linear(F.normalize(feature), F.normalize(self.weight))
+        phi = cosine - self.m
+        # --------------------------- convert label to one-hot ---------------------------
+        one_hot = torch.zeros(cosine.size(), device='cuda')
+        # one_hot = one_hot.cuda() if cosine.is_cuda else one_hot
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
+        output *= self.s
+        # print(output)
+
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + 'in_features=' + str(self.in_features) \
+               + ', out_features=' + str(self.out_features) \
+               + ', s=' + str(self.s) \
+               + ', m=' + str(self.m) + ')'
+
+
+class SphereProduct(nn.Module):
+    r"""Implement of large margin cosine distance: :
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        m: margin
+        cos(m*theta)
+    """
+    def __init__(self, in_features, out_features, m=4):
+        super(SphereProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.m = m
+        self.base = 1000.0
+        self.gamma = 0.12
+        self.power = 1
+        self.LambdaMin = 5.0
+        self.iter = 0
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform(self.weight)
+
+        # duplication formula
+        self.mlambda = [
+            lambda x: x ** 0,
+            lambda x: x ** 1,
+            lambda x: 2 * x ** 2 - 1,
+            lambda x: 4 * x ** 3 - 3 * x,
+            lambda x: 8 * x ** 4 - 8 * x ** 2 + 1,
+            lambda x: 16 * x ** 5 - 20 * x ** 3 + 5 * x
+        ]
+
+    def forward(self, feature, label):
+        # lambda = max(lambda_min,base*(1+gamma*iteration)^(-power))
+        self.iter += 1
+        self.lamb = max(self.LambdaMin, self.base * (1 + self.gamma * self.iter) ** (-1 * self.power))
+
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cos_theta = F.linear(F.normalize(feature), F.normalize(self.weight))
+        cos_theta = cos_theta.clamp(-1, 1)
+        cos_m_theta = self.mlambda[self.m](cos_theta)
+        theta = cos_theta.data.acos()
+        k = (self.m * theta / 3.14159265).floor()
+        phi_theta = ((-1.0) ** k) * cos_m_theta - 2 * k
+        NormOfFeature = torch.norm(input, 2, 1)
+
+        # --------------------------- convert label to one-hot ---------------------------
+        one_hot = torch.zeros(cos_theta.size())
+        one_hot = one_hot.cuda() if cos_theta.is_cuda else one_hot
+        one_hot.scatter_(1, label.view(-1, 1), 1)
+
+        # --------------------------- Calculate output ---------------------------
+        output = (one_hot * (phi_theta - cos_theta) / (1 + self.lamb)) + cos_theta
+        output *= NormOfFeature.view(-1, 1)
+
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + 'in_features=' + str(self.in_features) \
+               + ', out_features=' + str(self.out_features) \
+               + ', m=' + str(self.m) + ')'
